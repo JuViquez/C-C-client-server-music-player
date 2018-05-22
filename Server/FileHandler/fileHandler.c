222da9436cce5bf15155bc4525370e9eb917d29c
JuViquez <jviquez2@gmail.com>
2018-05-21 17:21:16 -0700

    file handler fixed

--- Server/FileHandler/fileHandler.c
+++ Server/FileHandler/fileHandler.c
@@ -7,7 +7,9 @@ void getDirFileList(int buff_size, int file_name_size, char buff[buff_size][file
   if (d) {
     for( int i = 0; i < buff_size; i++){
        if((dir = readdir(d)) != NULL){
-	strcpy(buff[i],dir->d_name);
+	if(strcmp(dir->d_name, ".") != 0 && strcmp(dir->d_name, "..") != 0){
+		strcpy(buff[i],dir->d_name);
+	}else i--;
        }else {
 	break;
        }
